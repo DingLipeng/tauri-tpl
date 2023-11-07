205efc3b48f92100bf68b5a2b0e38b89c308af11
Daniel Ting <danielting@aliyun.com>
2023-11-07 09:46:18 +0800

    chore: tauri升级1.5，窗口输出tauri版本

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,13 @@
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 import Greet from "./components/GreetItem.vue";
+import VersionItem from "./components/VersionItem.vue";
 </script>
 
 <template>
   <div class="container">
     <h1>A Tauri Template!</h1>
+    <version-item />
     <p>Step 0: Input a name to communicate with Rust backend</p>
     <Greet />
   </div>
---
+++ src/components/VersionItem.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import {ref} from "vue";
+import {getTauriVersion} from "@tauri-apps/api/app";
+
+const tauriVersion = ref("");
+getTauriVersion().then(version => {
+  tauriVersion.value = version
+})
+</script>
+
+<template>
+  <p>Tauri v{{ tauriVersion }}</p>
+</template>
